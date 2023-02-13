5a684c426b52975e2c40ef201ddb2aa297e74442
josefran14 <fransaavedra21@gmail.com>
2023-02-12 23:57:06 -0400

    Variant y component

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,11 @@
-import { Typography } from "@mui/material"
+import { Typography } from "@mui/material";
 
 export const JournalPage = () => {
   return (
     <>
-    <Typography variant='h1'>JournalPage</Typography>
+      <Typography variant="h1" component="span">JournalPage</Typography>
+      {/* <Button variant="contained" sx={{bgcolor: "red", color: 'green'}}>Text</Button>
+    <Box bgcolor="blue"><p>Oliver</p></Box> */}
     </>
-  )
-}
+  );
+};
