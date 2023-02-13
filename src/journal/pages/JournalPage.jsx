6783b66a6be153c1c4fd1d58581a1aa8661143d1
josefran14 <fransaavedra21@gmail.com>
2023-02-13 00:51:16 -0400

    Primera parte del diseño de la pagina del LoginPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 export const JournalPage = () => {
   return (
     <>
-      <Typography variant="h1" component="span">JournalPage</Typography>
+      <Typography variant="h1">JournalPage</Typography>
       {/* <Button variant="contained" sx={{bgcolor: "red", color: 'green'}}>Text</Button>
     <Box bgcolor="blue"><p>Oliver</p></Box> */}
     </>
