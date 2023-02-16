61dd7b79278160f4c4133d604e3c24c74eab2b2b
josefran14 <fransaavedra21@gmail.com>
2023-02-16 01:06:20 -0400

    Se realizo el diseño del SideBar de la pagina del JournalPage

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,7 +1,8 @@
-import { Box } from "@mui/material"
+import { Box, Toolbar } from "@mui/material"
 import { NavBar } from "../components/NavBar"
+import { SideBar } from "../components/SideBar"
 
-const drawerWidth = 240
+const drawerWidth = 280
 
 export const JournalLayout = ({children}) => {
   return (
@@ -9,11 +10,11 @@ export const JournalLayout = ({children}) => {
 
         <NavBar drawerWidth={drawerWidth}/>
 
-        {/* SideBar */}
+        <SideBar drawerWidth={drawerWidth}/>
 
         <Box component='main' sx={{flexGrow: 1, p: 3, }}>
 
-            {/* ToolBar */}
+           <Toolbar/>
 
             {children}
 
