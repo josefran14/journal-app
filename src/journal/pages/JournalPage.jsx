dc19024a401aeada601f5f54dac5f92cb2e5186b
josefran14 <fransaavedra21@gmail.com>
2023-02-16 01:45:35 -0400

    Se creo una view que es el componente NothingSelectedView el cual se va a mostrar cuando no haya nada seleccionado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
-import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views/NothingSelectedView";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores reprehenderit dignissimos ratione eaque excepturi praesentium consequatur architecto illum doloremque distinctio, eligendi et officiis recusandae voluptas ex voluptatem similique porro sequi!</Typography>
+      {/* <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores reprehenderit dignissimos ratione eaque excepturi praesentium consequatur architecto illum doloremque distinctio, eligendi et officiis recusandae voluptas ex voluptatem similique porro sequi!</Typography> */}
 
-      {/* NothingSelected */}
+       <NothingSelectedView/>
       {/* NoteView */}
 
     </JournalLayout>
