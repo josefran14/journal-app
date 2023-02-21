54929db356c988713a2e25826643029a5f529b3d
josefran14 <fransaavedra21@gmail.com>
2023-02-21 13:41:44 -0400

    Se creo un boton flotante en la pagina JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView } from "../views/NoteView";
 import { NothingSelectedView } from "../views/NothingSelectedView";
@@ -10,9 +12,23 @@ export const JournalPage = () => {
 
       {/* <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores reprehenderit dignissimos ratione eaque excepturi praesentium consequatur architecto illum doloremque distinctio, eligendi et officiis recusandae voluptas ex voluptatem similique porro sequi!</Typography> */}
 
-       {/* <NothingSelectedView drawerWidth={drawerWidth}/> */}
+      <NothingSelectedView drawerWidth={drawerWidth}/> 
       
-      <NoteView/>
+      {/* <NoteView/> */}
+
+      <IconButton
+      size='large'
+      sx={{
+        color: 'white',
+        backgroundColor: 'error.main',
+        ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+        position: 'fixed',
+        right: 50,
+        bottom: 50
+      }}
+      >
+        <AddOutlined sx={{fontSize: 30}}/>
+      </IconButton>
 
     </JournalLayout>
   );
